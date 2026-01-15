03856619de85d42d7d52a346827510f30c7d47e5
Abubakar Shehu <shehu.lanre@icloud.com>
2026-01-15 09:43:06 -0700

    First coding class

--- 01 - javascript review/examples/01 - simple review example/student/js/index.js
+++ 01 - javascript review/examples/01 - simple review example/student/js/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /*
 Enter JS here
 
@@ -6,3 +7,28 @@ HTML for list topic list item
     NEW TOPIC HERE
 </li>
 */
+// Select topic list & form
+
+let topicForm = document.querySelector(".new-topic-form");
+let topicList = document.querySelector(".topics-list");
+
+// Create function to add text input to topic list
+let addTopicToPage = (topicName, topicListElement) => {
+	let newTopicElement = `<li class="list-group-item">${topicName}</li>`;
+	topicListElement.innerHTML += newTopicElement;
+};
+
+topicForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  let topicInput = event.target.elements["new-topic"];
+	let topicText = topicInput.value;
+	
+	if (topicText === "") {
+		topicInput.classList.addEventListener("is-invalid");
+	} else {
+		topicInput.classList.remove("is-invalid");
+	}
+
+	addTopicToPage(topicText, topicList);
+});
